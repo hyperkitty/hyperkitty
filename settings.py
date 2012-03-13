7519273d72ff5de350a0cab11b9d0bac28452da0
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-03-13 10:20:21 +0100

    Adjust settings to remove reference to mm_app

--- settings.py
+++ settings.py
@@ -109,7 +109,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'mm_app.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
