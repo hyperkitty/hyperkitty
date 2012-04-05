0e5289c9bc7edf80640c4cef35789435b2ab6c46
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-04-05 23:58:01 +0200

    Some formating

--- urls.py
+++ urls.py
@@ -14,39 +14,52 @@ urlpatterns = patterns('',
     url(r'^$', 'views.pages.index'),
 
     ################# ARCHIVES ###################
-    url(r'^archives/(?P<mlist_fqdn>.*@.*)/(?P<year>\d{4})/(?P<month>\d\d?)/$', 'views.pages.archives'),
-    url(r'^archives/(?P<mlist_fqdn>.*@.*)/$', 'views.pages.archives'),
+    url(r'^archives/(?P<mlist_fqdn>.*@.*)/(?P<year>\d{4})/(?P<month>\d\d?)/$',
+        'views.pages.archives'),
+    url(r'^archives/(?P<mlist_fqdn>.*@.*)/$',
+        'views.pages.archives'),
 
     ############### THREAD VIEW ##################
-    url(r'^thread/(?P<mlist_fqdn>.*@.*)/(?P<threadid>.+)/$', 'views.pages.thread'),
+    url(r'^thread/(?P<mlist_fqdn>.*@.*)/(?P<threadid>.+)/$',
+        'views.pages.thread'),
 
 
     ############### LIST VIEW   ###################
     url(r'^list/$', 'views.pages.index'),
-    url(r'^list/(?P<mlist_fqdn>.*@.*)/$', 'views.pages.list'),
+    url(r'^list/(?P<mlist_fqdn>.*@.*)/$',
+        'views.pages.list'),
 
 
     ############### MESSAGE ########################
-    url(r'^message/(?P<mlist_fqdn>.*@.*)/(?P<messageid>.+)/$', 'views.pages.message'),
+    url(r'^message/(?P<mlist_fqdn>.*@.*)/(?P<messageid>.+)/$',
+        'views.pages.message'),
 
 
     ############### SEARCH ##################
     # If page number is present in URL
-    url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)\/(?P<page>\d+)/$', 'views.pages.search_keyword'),
+    url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)\/(?P<page>\d+)/$',
+        'views.pages.search_keyword'),
     # Show the first page as default when no page number is present in URL
-    url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)/$', 'views.pages.search_keyword'),
-    url(r'^search/(?P<mlist_fqdn>.*@.*)/$', 'views.pages.search'),
+    url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)/$',
+        'views.pages.search_keyword'),
+    url(r'^search/(?P<mlist_fqdn>.*@.*)/$',
+        'views.pages.search'),
 
 
     ############## TAG   ######################
-    url(r'^tag/(?P<mlist_fqdn>.*@.*)\/(?P<tag>.*)\/(?P<page>\d+)/$', 'views.pages.search_tag'),
-    url(r'^tag/(?P<mlist_fqdn>.*@.*)\/(?P<tag>.*)/$', 'views.pages.search_tag'),
+    url(r'^tag/(?P<mlist_fqdn>.*@.*)\/(?P<tag>.*)\/(?P<page>\d+)/$',
+        'views.pages.search_tag'),
+    url(r'^tag/(?P<mlist_fqdn>.*@.*)\/(?P<tag>.*)/$',
+        'views.pages.search_tag'),
 
     ############## REST API #####################
     url(r'^api/$', 'views.pages.api'),
-    url(r'^api/email\/(?P<mlist_fqdn>.*@.*)\/(?P<messageid>.*)/',  EmailResource.as_view()),
-    url(r'^api/thread\/(?P<mlist_fqdn>.*@.*)\/(?P<threadid>.*)/',  ThreadResource.as_view()),
-    url(r'^api/search\/(?P<mlist_fqdn>.*@.*)\/(?P<field>.*)\/(?P<keyword>.*)/',  SearchResource.as_view()),
+    url(r'^api/email\/(?P<mlist_fqdn>.*@.*)\/(?P<messageid>.*)/',
+        EmailResource.as_view()),
+    url(r'^api/thread\/(?P<mlist_fqdn>.*@.*)\/(?P<threadid>.*)/',
+        ThreadResource.as_view()),
+    url(r'^api/search\/(?P<mlist_fqdn>.*@.*)\/(?P<field>.*)\/(?P<keyword>.*)/',
+        SearchResource.as_view()),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
