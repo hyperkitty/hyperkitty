99739ac77e13ebb5c71d1f616ea4d5052109f005
Aamir Khan <syst3m.w0rm@gmail.com>
2012-04-06 00:51:00 +0530

    removed urls without trailing slash from urls.py

--- urls.py
+++ urls.py
@@ -9,52 +9,44 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'hyperkitty.views.home', name='home'),
-    # url(r'^hyperkitty/', include('hyperkitty.foo.urls')),
-    # This will be the new index page
-    url(r'^$', 'views.pages.index'),
+    ################# INDEX PAGE ################
     url(r'^/$', 'views.pages.index'),
-    # This will be the new archives page
+    url(r'^$', 'views.pages.index'),
+
+    ################# ARCHIVES ###################
     url(r'^archives/(?P<mlist_fqdn>.*@.*)/(?P<year>\d{4})/(?P<month>\d\d?)/$', 'views.pages.archives'),
-    url(r'^archives/(?P<mlist_fqdn>.*@.*)/(?P<year>\d{4})/(?P<month>\d\d?)$', 'views.pages.archives'),
     url(r'^archives/(?P<mlist_fqdn>.*@.*)/$', 'views.pages.archives'),
-    url(r'^archives/(?P<mlist_fqdn>.*@.*)$', 'views.pages.archives'),
-    # The thread view
-    url(r'^thread/(?P<mlist_fqdn>.*@.*)/(?P<threadid>.+)$', 'views.pages.thread'),
-    # This will be the new recent page
-    url(r'^list$', 'views.pages.index'),
+
+    ############### THREAD VIEW ##################
+    url(r'^thread/(?P<mlist_fqdn>.*@.*)/(?P<threadid>.+)/$', 'views.pages.thread'),
+
+
+    ############### LIST VIEW   ###################
     url(r'^list/$', 'views.pages.index'),
     url(r'^list/(?P<mlist_fqdn>.*@.*)/$', 'views.pages.list'),
-    url(r'^list/(?P<mlist_fqdn>.*@.*)$', 'views.pages.list'),
-    # Single message view
-    url(r'^message/(?P<mlist_fqdn>.*@.*)/(?P<messageid>.+)$', 'views.pages.message'),
-    # Search
-    #url(r'^search$', 'views.pages.search'),
+
+
+    ############### MESSAGE ########################
+    url(r'^message/(?P<mlist_fqdn>.*@.*)/(?P<messageid>.+)/$', 'views.pages.message'),
+
+
+    ############### SEARCH ##################
     # If page number is present in URL
     url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)\/(?P<page>\d+)/$', 'views.pages.search_keyword'),
-    url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)\/(?P<page>\d+)$', 'views.pages.search_keyword'),
     # Show the first page as default when no page number is present in URL
     url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)/$', 'views.pages.search_keyword'),
-    url(r'^search/(?P<mlist_fqdn>.*@.*)\/(?P<target>.*)\/(?P<keyword>.*)$', 'views.pages.search_keyword'),
-    
     url(r'^search/(?P<mlist_fqdn>.*@.*)/$', 'views.pages.search'),
-    url(r'^search/(?P<mlist_fqdn>.*@.*)$', 'views.pages.search'),
-    url(r'^tag/(?P<mlist_fqdn>.*@.*)\/(?P<tag>.*)$', 'views.pages.search_tag'),
-    # mockups:
-    url(r'^mockup/$', 'views.mockup.index'),
-    url(r'^mockup/archives$', 'views.mockup.archives'),
-    url(r'^mockup/archives/(?P<year>\d{4})/(?P<month>\d{2})/$', 'views.mockup.archives'),
-    url(r'^mockup/recent$', 'views.mockup.recent'),
-    url(r'^mockup/search$', 'views.mockup.search'),
-    url(r'^mockup/search\/(?P<keyword>.*)$', 'views.mockup.search_keyword'),
-    url(r'^mockup/tag\/(?P<tag>.*)$', 'views.mockup.search_tag'),
 
-    # REST API
+
+    ############## TAG   ######################
+    url(r'^tag/(?P<mlist_fqdn>.*@.*)\/(?P<tag>.*)/$', 'views.pages.search_tag'),
+
+    ############## REST API #####################
     url(r'^api/$', 'views.pages.api'),
     url(r'^api/email\/(?P<mlist_fqdn>.*@.*)\/(?P<messageid>.*)/',  EmailResource.as_view()),
     url(r'^api/thread\/(?P<mlist_fqdn>.*@.*)\/(?P<threadid>.*)/',  ThreadResource.as_view()),
     url(r'^api/search\/(?P<mlist_fqdn>.*@.*)\/(?P<field>.*)\/(?P<keyword>.*)/',  SearchResource.as_view()),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
