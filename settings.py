e1e210d0336224c3ab80cf72fc5f02659fd43c24
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-05-14 21:54:40 +0200

    Adjust settings to the name of the module

--- settings.py
+++ settings.py
@@ -129,7 +129,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'mongodb',
+    'rdbms',
     'djangorestframework',
     'gravatar',
 )
