0e4bf7c225fe2b68296f3a644755b49104459c14
Aamir Khan <syst3m.w0rm@gmail.com>
2012-06-11 18:14:11 -0400

    Basic configuration stuff

--- settings.py
+++ settings.py
@@ -119,17 +119,16 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-#    'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
 #    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
+    'django.contrib.admin',
     # 'django.contrib.admindocs',
     'rdbms',
+    'social_auth',
     'djangorestframework',
     'gravatar',
 )
--- urls.py
+++ urls.py
@@ -5,8 +5,8 @@ from api import EmailResource, ThreadResource, SearchResource
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Index
@@ -72,8 +72,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Admin  
+    url(r'^admin/', include(admin.site.urls)),
 )
 #) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
