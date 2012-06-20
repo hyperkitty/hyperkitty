43e50062e50495d0cf0ff1e66a7d5bfac16629c8
Aamir Khan <syst3m.w0rm@gmail.com>
2012-06-20 01:09:33 -0400

    messageid was getting truncated because of insufficient length

--- models.py
+++ models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 class Rating(models.Model):
 	list_address = models.CharField(max_length=50)
-	messageid = models.CharField(max_length=30)
+	messageid = models.CharField(max_length=100)
 	user = models.ForeignKey(User)
 	vote = models.SmallIntegerField()
