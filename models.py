af0900fc6ce9f52347265fd5b899004554cb07a8
Aamir Khan <syst3m.w0rm@gmail.com>
2012-06-20 01:08:56 -0400

    user should not be unique in ratings table

--- models.py
+++ models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class Rating(models.Model):
 	list_address = models.CharField(max_length=50)
 	messageid = models.CharField(max_length=30)
-	user = models.ForeignKey(User, unique=True)
+	user = models.ForeignKey(User)
 	vote = models.SmallIntegerField()
