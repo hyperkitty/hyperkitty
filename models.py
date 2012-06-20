5157cc1218f2e758c52f3aba3fcd538b9d442b9c
Aamir Khan <syst3m.w0rm@gmail.com>
2012-06-20 01:47:48 -0400

    Adding UserProfile Model

--- models.py
+++ models.py
@@ -1,8 +1,19 @@
+import logging
+
 from django.db import models
 from django.contrib.auth.models import User
+
+logger = logging.getLogger(__name__)
+
 
 class Rating(models.Model):
 	list_address = models.CharField(max_length=50)
 	messageid = models.CharField(max_length=100)
 	user = models.ForeignKey(User)
 	vote = models.SmallIntegerField()
+
+class UserProfile(models.Model):
+	# User Object
+	user = models.OneToOneField(User)
+	
+	karma = models.IntegerField()
