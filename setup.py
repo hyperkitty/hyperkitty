9f18a590819a01017c15169d82763680a72848fb
Aamir Khan <syst3m.w0rm@gmail.com>
2012-07-24 17:00:25 -0400

    Packaging hyperkitty

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name="HyperKitty",
+    version="0.1",
+    description="A web interface to access GNU Mailman archives", 
+    long_description=open('README.rst').read(),
+    url="https://fedorahosted.org/hyperkitty/",
+    packages=find_packages(exclude=["*.test", "test", "*.test.*"]),
+    include_package_data=True,
+    install_requires=open('requirements.txt').read(),
+    )
