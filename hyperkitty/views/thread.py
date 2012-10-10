ac497191016711fe04065bf13286ba82ee825e0a
Aurélien Bompard <aurelien@bompard.org>
2012-10-10 19:15:57 +0200

    Fix a few remaining bugs

--- hyperkitty/views/thread.py
+++ hyperkitty/views/thread.py
@@ -75,7 +75,7 @@ def thread_index (request, mlist_fqdn, threadid):
         message.likestatus = "neutral"
         if message.likes - message.dislikes >= 10:
             message.likestatus = "likealot"
-        elif thread.likes - message.dislikes > 0:
+        elif message.likes - message.dislikes > 0:
             message.likestatus = "like"
         #elif message.likes - message.dislikes < 0:
         #    message.likestatus = "dislike"
