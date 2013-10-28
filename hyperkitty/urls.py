85037f95b2b22a812938dc19aa2a9da35a6ea46f
Aurélien Bompard <aurelien@bompard.org>
2013-10-28 18:00:42 +0100

    Rename the index view module

--- hyperkitty/urls.py
+++ hyperkitty/urls.py
@@ -38,9 +38,9 @@ from hyperkitty.views import TextTemplateView
 
 urlpatterns = patterns('hyperkitty.views',
     # Index
-    url(r'^/$', 'pages.index', name='index'),
-    url(r'^$', 'pages.index', name='root'),
-    url(r'^lists-properties$', 'pages.list_properties', name='list_properties'),
+    url(r'^/$', 'index.index', name='index'),
+    url(r'^$', 'index.index', name='root'),
+    url(r'^lists-properties$', 'index.list_properties', name='list_properties'),
 
     # Account
     url(r'^accounts/login/$', 'accounts.login_view', {'template_name': 'login.html', 'SSL': True}, name='user_login'),
