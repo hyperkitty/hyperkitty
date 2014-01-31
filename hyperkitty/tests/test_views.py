7b683a31153c1d0972f29bb9974e26bf2836e2d0
Aurélien Bompard <aurelien@bompard.org>
2014-01-31 14:23:28 +0100

    Use the test client instead of the factory

--- hyperkitty/tests/test_views.py
+++ hyperkitty/tests/test_views.py
@@ -28,8 +28,7 @@ from mock import Mock
 
 import django.utils.simplejson as json
 from hyperkitty.tests.utils import TestCase
-from django.test.client import RequestFactory
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from mailman.email.message import Message
 from mailman.interfaces.archiver import ArchivePolicy
@@ -40,7 +39,6 @@ from kittystore.test import FakeList, SettingsModule
 
 from hyperkitty.models import LastView
 
-from hyperkitty.views.accounts import user_profile, user_registration
 
 
 def genericAssertRedirects(self, response, target):
@@ -51,52 +49,43 @@ def genericAssertRedirects(self, response, target):
     self.assertEqual(response._headers["location"][1], target)
 
 
-class AccountViewsTestCase(TestCase):
 
-    assertRedirects = genericAssertRedirects
+class AccountViewsTestCase(TestCase):
 
     def setUp(self):
         store = kittystore.get_store(SettingsModule(),
                                      debug=False, auto_create=True)
-        defaults = {"kittystore.store": store, "HTTP_USER_AGENT": "testbot"}
-        self.factory = RequestFactory(**defaults)
+        self.client.defaults = {"kittystore.store": store,
+                                "HTTP_USER_AGENT": "testbot",
+                                }
+        self.user = User.objects.create_user('testuser', 'test@example.com', 'testPass')
 
     def test_login(self):
         # Try to access user profile (private data) without logging in
-        request = self.factory.get(reverse(user_profile))
-        request.user = AnonymousUser()
-        response = user_profile(request)
+        response = self.client.get(reverse("user_profile"))
         self.assertRedirects(response,
-                "%s?next=%s" % (reverse('user_login'), reverse(user_profile)))
+                "%s?next=%s" % (reverse('user_login'), reverse("user_profile")))
 
     def test_profile(self):
-        request = self.factory.get(reverse(user_profile))
-        request.user = User.objects.create_user('testuser', 'test@example.com', 'testPass')
-        response = user_profile(request)
+        self.client.login(username='testuser', password='testPass')
+        response = self.client.get(reverse("user_profile"))
         self.assertEqual(response.status_code, 200)
 
     def test_registration(self):
-        request = self.factory.get(reverse(user_registration))
-        request.user = User.objects.create_user('testuser', 'test@example.com', 'testPass')
-        response = user_registration(request)
-        #self.client.login(username='testuser', password='testPass')
-
+        self.client.login(username='testuser', password='testPass')
         # If the user if already logged in, redirect to index page...
         # Don't let him register again
-        #response = self.client.get(reverse('user_registration'))
+        response = self.client.get(reverse('user_registration'))
         self.assertRedirects(response, reverse('root'))
-        #self.client.logout()
-        request.user = AnonymousUser()
 
         # Access the user registration page after logging out and try to register now
-        #response = self.client.get(reverse('user_registration'))
-        response = user_registration(request)
+        self.client.logout()
+        response = self.client.get(reverse('user_registration'))
         self.assertEqual(response.status_code, 200)
 
         # @TODO: Try to register a user and verify its working
 
 
-from hyperkitty.views.thread import thread_index
 
 class LastViewsTestCase(TestCase):
 
@@ -161,6 +150,7 @@ class LastViewsTestCase(TestCase):
         response = self.client.get(reverse('list_overview', args=["list@example.com"]))
         self.assertContains(response, "icon-eye-close",
                             count=4, status_code=200)
+
 
 
 class MessageViewsTestCase(TestCase):
@@ -244,40 +234,46 @@ class MessageViewsTestCase(TestCase):
 
 
 
-from hyperkitty.views.list import archives
-
 class ListArchivesTestCase(TestCase):
 
     def setUp(self):
-#        self.store = Mock()
-#        self.store.get_threads.return_value = []
-#        self.store.get_list.side_effect = lambda n: FakeList(n)
-#        defaults = {"kittystore.store": self.store}
-#        self.factory = RequestFactory(**defaults)
-        self.factory = RequestFactory()
+        store = kittystore.get_store(SettingsModule(),
+                                     debug=False, auto_create=True)
+        self.client.defaults = {"kittystore.store": store,
+                                "HTTP_USER_AGENT": "testbot",
+                                }
+        # Create the list by adding a dummy message
+        ml = FakeList("list@example.com")
+        msg = Message()
+        msg["From"] = "dummy@example.com"
+        msg["Message-ID"] = "<msg>"
+        msg.set_payload("Dummy message")
+        store.add_to_list(ml, msg)
 
     def test_no_date(self):
         today = datetime.date.today()
-        request = self.factory.get("/archives")
-        response = archives(request, 'list@example.com')
+        response = self.client.get(reverse(
+                'archives_latest', args=['list@example.com']))
         final_url = reverse('archives_with_month',
                 kwargs={'mlist_fqdn': 'list@example.com',
                         'year': today.year,
                         'month': today.month,
                 })
-        self.assertEqual(response["location"], final_url)
+        self.assertRedirects(response, final_url)
 
 
-
-from hyperkitty.views.thread import reattach, reattach_suggest
 
 class ReattachTestCase(TestCase):
 
     def setUp(self):
         self.user = User.objects.create_user('testuser', 'test@example.com', 'testPass')
         self.user.is_staff = True
+        self.user.save()
         self.client.login(username='testuser', password='testPass')
         self.store = kittystore.get_store(SettingsModule(), debug=False, auto_create=True)
+        self.client.defaults = {"kittystore.store": self.store,
+                                "HTTP_USER_AGENT": "testbot",
+                                }
         ml = FakeList("list@example.com")
         ml.subject_prefix = u"[example] "
         # Create 2 threads
@@ -290,18 +286,14 @@ class ReattachTestCase(TestCase):
             msg.set_payload("Dummy message")
             msg["Message-ID-Hash"] = self.store.add_to_list(ml, msg)
             self.messages.append(msg)
-        # Factory
-        defaults = {"kittystore.store": self.store, "HTTP_USER_AGENT": "testbot"}
-        self.factory = RequestFactory(**defaults)
 
     def test_suggestions(self):
         threadid = self.messages[0]["Message-ID-Hash"]
-        request = self.factory.get(reverse('thread_reattach_suggest',
-                                   args=["list@example.com", threadid]))
         msg2 = self.store.get_message_by_id_from_list("list@example.com", "id2")
         self.store.search = Mock(return_value={"results": [msg2]})
         self.store.search_index = True
-        response = reattach_suggest(request, "list@example.com", threadid)
+        response = self.client.get(reverse('thread_reattach_suggest',
+                                   args=["list@example.com", threadid]))
         self.store.search.assert_called_with(
             u'dummy message', 'list@example.com', 1, 50)
         other_threadid = self.messages[1]["Message-ID-Hash"]
@@ -311,11 +303,9 @@ class ReattachTestCase(TestCase):
     def test_reattach(self):
         threadid1 = self.messages[0]["Message-ID-Hash"]
         threadid2 = self.messages[1]["Message-ID-Hash"]
-        request = self.factory.post(reverse('thread_reattach',
-                                    args=["list@example.com", threadid2]),
-                                    data={"parent": threadid1})
-        request.user = self.user
-        response = reattach(request, "list@example.com", threadid2)
+        response = self.client.post(reverse('thread_reattach',
+                                   args=["list@example.com", threadid2]),
+                                   data={"parent": threadid1})
         now = datetime.datetime.now()
         threads = list(self.store.get_threads("list@example.com",
                 now - datetime.timedelta(days=1),
@@ -323,17 +313,15 @@ class ReattachTestCase(TestCase):
         self.assertEqual(len(threads), 1)
         self.assertEqual(threads[0].thread_id, threadid1)
         expected_url = reverse('thread', args=["list@example.com", threadid1]) + "?msg=attached-ok"
-        self.assertEqual(response["location"], expected_url)
+        self.assertRedirects(response, expected_url)
 
     def test_reattach_manual(self):
         threadid1 = self.messages[0]["Message-ID-Hash"]
         threadid2 = self.messages[1]["Message-ID-Hash"]
-        request = self.factory.post(reverse('thread_reattach',
+        response = self.client.post(reverse('thread_reattach',
                                     args=["list@example.com", threadid2]),
                                     data={"parent": "",
                                           "parent-manual": threadid1})
-        request.user = self.user
-        response = reattach(request, "list@example.com", threadid2)
         now = datetime.datetime.now()
         threads = list(self.store.get_threads("list@example.com",
                 now - datetime.timedelta(days=1),
@@ -341,16 +329,14 @@ class ReattachTestCase(TestCase):
         self.assertEqual(len(threads), 1)
         self.assertEqual(threads[0].thread_id, threadid1)
         expected_url = reverse('thread', args=["list@example.com", threadid1]) + "?msg=attached-ok"
-        self.assertEqual(response["location"], expected_url)
+        self.assertRedirects(response, expected_url)
 
     def test_reattach_invalid(self):
         threadid = self.messages[0]["Message-ID-Hash"]
-        request = self.factory.post(reverse('thread_reattach',
+        self.store.attach_to_thread = Mock()
+        response = self.client.post(reverse('thread_reattach',
                                     args=["list@example.com", threadid]),
                                     data={"parent": "invalid-data"})
-        request.user = self.user
-        self.store.attach_to_thread = Mock()
-        response = reattach(request, "list@example.com", threadid)
         self.assertFalse(self.store.attach_to_thread.called)
         now = datetime.datetime.now()
         threads = list(self.store.get_threads("list@example.com",
@@ -363,12 +349,10 @@ class ReattachTestCase(TestCase):
 
     def test_reattach_on_itself(self):
         threadid = self.messages[0]["Message-ID-Hash"]
-        request = self.factory.post(reverse('thread_reattach',
+        self.store.attach_to_thread = Mock()
+        response = self.client.post(reverse('thread_reattach',
                                     args=["list@example.com", threadid]),
                                     data={"parent": threadid})
-        request.user = self.user
-        self.store.attach_to_thread = Mock()
-        response = reattach(request, "list@example.com", threadid)
         self.assertFalse(self.store.attach_to_thread.called)
         now = datetime.datetime.now()
         threads = list(self.store.get_threads("list@example.com",
@@ -382,12 +366,10 @@ class ReattachTestCase(TestCase):
     def test_reattach_on_unknown(self):
         threadid = self.messages[0]["Message-ID-Hash"]
         threadid_unknown = "L36TVP2EFFDSXGVNQJCY44W5AB2YMJ65"
-        request = self.factory.post(reverse('thread_reattach',
+        self.store.attach_to_thread = Mock()
+        response = self.client.post(reverse('thread_reattach',
                                     args=["list@example.com", threadid]),
                                     data={"parent": threadid_unknown})
-        request.user = self.user
-        self.store.attach_to_thread = Mock()
-        response = reattach(request, "list@example.com", threadid)
         self.assertFalse(self.store.attach_to_thread.called)
         self.assertContains(response, '<div class="alert alert-warning">',
                 count=1, status_code=200)
@@ -396,12 +378,10 @@ class ReattachTestCase(TestCase):
     def test_reattach_old_to_new(self):
         threadid1 = self.messages[0]["Message-ID-Hash"]
         threadid2 = self.messages[1]["Message-ID-Hash"]
-        request = self.factory.post(reverse('thread_reattach',
+        self.store.attach_to_thread = Mock()
+        response = self.client.post(reverse('thread_reattach',
                                     args=["list@example.com", threadid1]),
                                     data={"parent": threadid2})
-        request.user = self.user
-        self.store.attach_to_thread = Mock()
-        response = reattach(request, "list@example.com", threadid1)
         self.assertFalse(self.store.attach_to_thread.called)
         now = datetime.datetime.now()
         threads = list(self.store.get_threads("list@example.com",
@@ -415,20 +395,17 @@ class ReattachTestCase(TestCase):
 
 
 
-from hyperkitty.views.list import archives, overview
-from hyperkitty.views.message import index as message_view
-from hyperkitty.views.search import search as search_view
-
 class PrivateArchivesTestCase(TestCase):
 
     def setUp(self):
         self.tmpdir = mkdtemp(prefix="hyperkitty-testing-")
         self.user = User.objects.create_user('testuser', 'test@example.com', 'testPass')
-        #self.user.is_staff = True
-        #self.client.login(username='testuser', password='testPass')
         settings = SettingsModule()
         settings.KITTYSTORE_SEARCH_INDEX = self.tmpdir
         self.store = kittystore.get_store(settings, debug=False, auto_create=True)
+        self.client.defaults = {"kittystore.store": self.store,
+                                "HTTP_USER_AGENT": "testbot",
+                                }
         ml = FakeList("list@example.com")
         ml.subject_prefix = u"[example] "
         ml.archive_policy = ArchivePolicy.private
@@ -438,53 +415,42 @@ class PrivateArchivesTestCase(TestCase):
         msg["Subject"] = "Dummy message"
         msg.set_payload("Dummy message")
         msg["Message-ID-Hash"] = self.msgid = self.store.add_to_list(ml, msg)
-        # Factory
-        defaults = {"kittystore.store": self.store,
-                    "HTTP_USER_AGENT": "testbot",
-                    "session": {}, }
-        self.factory = RequestFactory(**defaults)
 
     def tearDown(self):
         rmtree(self.tmpdir)
 
+
+    def _do_test(self, url, query={}):
+        response = self.client.get(url, query)
+        self.assertEquals(response.status_code, 403)
+        self.client.login(username='testuser', password='testPass')
+        # use a temp variable below because self.client.session is actually a
+        # property which returns a new instance en each call :-/
+        session = self.client.session
+        session["subscribed"] = ["list@example.com"]
+        session.save()
+        response = self.client.get(url, query)
+        self.assertContains(response, "Dummy message", status_code=200)
+
+
     def test_month_view(self):
         now = datetime.datetime.now()
-        request = self.factory.get(reverse('archives_with_month', args=["list@example.com", now.year, now.month]))
-        self._do_test(request, archives, ["list@example.com", now.year, now.month])
+        self._do_test(reverse('archives_with_month', args=["list@example.com", now.year, now.month]))
 
     def test_overview(self):
-        request = self.factory.get(reverse('list_overview', args=["list@example.com"]))
-        self._do_test(request, overview, ["list@example.com"])
+        self._do_test(reverse('list_overview', args=["list@example.com"]))
 
     def test_thread_view(self):
-        request = self.factory.get(reverse('thread',
-                    args=["list@example.com", self.msgid]))
-        self._do_test(request, thread_index, ["list@example.com", self.msgid])
+        self._do_test(reverse('thread', args=["list@example.com", self.msgid]))
 
     def test_message_view(self):
-        request = self.factory.get(reverse('message_index',
-                    args=["list@example.com", self.msgid]))
-        self._do_test(request, message_view, ["list@example.com", self.msgid])
+        self._do_test(reverse('message_index', args=["list@example.com", self.msgid]))
 
     def test_search_list(self):
-        request = self.factory.get(reverse('search'),
-                    {"list": "list@example.com", "query": "dummy"})
-        self._do_test(request, search_view)
+        self._do_test(reverse('search'), {"list": "list@example.com", "query": "dummy"})
 
     def test_search_all_lists(self):
         # When searching all lists, we only search public lists regardless of
         # the user's subscriptions
-        request = self.factory.get(reverse('search'), {"query": "dummy"})
-        request.user = AnonymousUser()
-        response = search_view(request)
+        response = self.client.get(reverse('search'), {"query": "dummy"})
         self.assertNotContains(response, "Dummy message", status_code=200)
-
-    def _do_test(self, request, view, view_args=[]):
-        #self.assertRaises(HttpForbidden, view, request, *view_args)
-        request.user = AnonymousUser()
-        response = view(request, *view_args)
-        self.assertEquals(response.status_code, 403)
-        request.user = self.user
-        setattr(request, "session", {"subscribed": ["list@example.com"]})
-        response = view(request, *view_args)
-        self.assertContains(response, "Dummy message", status_code=200)
