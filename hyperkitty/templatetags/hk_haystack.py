7b4739eb8eab5c3e4ec38a19ad3f22133b351ee0
Aurélien Bompard <aurelien@bompard.org>
2015-03-06 16:23:32 +0100

    Remove terms longer than 255 chars or Xapian will choke on them

---
+++ hyperkitty/templatetags/hk_haystack.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 1998-2012 by the Free Software Foundation, Inc.
+#
+# This file is part of HyperKitty.
+#
+# HyperKitty is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# HyperKitty is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# HyperKitty.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Aurelien Bompard <abompard@fedoraproject.org>
+#
+
+from django import template
+register = template.Library()
+
+@register.filter
+def nolongterms(text):
+    """
+    Remove terms longer than 255 chars, or Xapian will choke on them:
+    https://github.com/notanumber/xapian-haystack/issues/77
+    """
+    ' '.join(word for word in text.split() if len(word) < 255)
